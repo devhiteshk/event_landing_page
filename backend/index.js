55c2c0d2fb33c862a8dbcddf4b6065e1263a0a9a
devhiteshk <dev.hitesh.k@gmail.com>
2023-01-18 23:44:48 +0530

    add cors

--- backend/index.js
+++ backend/index.js
@@ -3,11 +3,15 @@ const mongoose = require("mongoose");
 const bodyParser = require("body-parser");
 const path = require("path");
 
+const cors = require("cors");
+
 require("dotenv").config();
 
 const app = express();
 
+app.use(cors);
 app.use(express.json());
+
 app.use(bodyParser.urlencoded({ extended: true }));
 
 const Schema = mongoose.Schema;
